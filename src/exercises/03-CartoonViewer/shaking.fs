d06b58ec3fe3dee8877636763447f8c43b1e9dec
ErwanLucas <erwan.lucas@polytechnique.edu>
2012-11-29 09:40:16 +0100

    Update shaking.fs for amplitude variation

--- src/exercises/03-CartoonViewer/shaking.fs
+++ src/exercises/03-CartoonViewer/shaking.fs
@@ -13,8 +13,8 @@ void main()
 	for( int i=0; i<N; i++ )
 	{
 		phi += 0.7;
-		displacement.x = 0.002*(1.+cos(200.*position.x + phi));
-		displacement.y = 0.002*(1.+cos(200.*position.y+ phi));
+		displacement.x = (0.002+phi/5000.)*(1.+cos(200.*position.x + phi));
+		displacement.y = (0.002+phi/5000.)*(1.+cos(200.*position.y+ phi));
 
 		edgeColor += texture2D(texture, gl_TexCoord[0].xy + displacement.xy);
 	
